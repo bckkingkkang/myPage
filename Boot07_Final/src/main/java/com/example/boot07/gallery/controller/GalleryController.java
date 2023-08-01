a3640b4a2b21b1d427cdb009bbcbeebdd72c7c69
가현 <bufar1014@gmail.com>
2023-08-01 12:50:59 +0900

    GalleryController added

--- Boot07_Final/src/main/java/com/example/boot07/gallery/controller/GalleryController.java
+++ Boot07_Final/src/main/java/com/example/boot07/gallery/controller/GalleryController.java
@@ -4,6 +4,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +16,12 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.example.boot07.gallery.dto.GalleryDto;
 import com.example.boot07.gallery.service.GalleryService;
 
 @Controller
@@ -40,5 +48,56 @@ public class GalleryController {
 	
 	// gallery 게시물의 num이 parameter get 방식으로 넘겨줌
 	// detail page
+	@RequestMapping(value="/gallery/detail", method=RequestMethod.POST)
+	public ModelAndView detail(ModelAndView mView, int num) {
+		// Gallery detail 페이지에 필요한 data를 num으로 가져와 ModelAndView 에 저장
+		service.getDetail(mView, num);
+		mView.setViewName("gallery/detail");
+		return mView;
+	}
 	
+	// gallery 사진 업로드 & DB 저장
+	@RequestMapping(method=RequestMethod.POST, value="/gallery/upload")
+	public String upload(GalleryDto dto, HttpServletRequest request) {
+		// form에서 dto로 데이터 받아옴
+		// dto : caption, MultipartFile image 를 가지고 있음
+		// request : imagePath 만드는 데 사용, session 영역의 id를 가져오는 데에 사용
+		service.saveImage(dto, request);
+		return "gallery/upload";
+	}
+	
+	// gallery 사진 업로드 form 페이지로 이동
+	@RequestMapping("/gallery/upload_form")
+	public String uploadForm() {
+		return "gallery/upload_form";
+	}
+	
+	// gallery 사진 업로드 form 페이지로 이동
+	@RequestMapping("/gallery/upload_form2")
+	public String uploadForm2() {
+		return "/gallery/upload_form2";
+	}
+	
+	// gallery 사진 업로드 form 페이지로 이동
+	@RequestMapping("/gallery/upload_form3")
+	public String uploadForm3() {
+		return "/gallery/upload_form3";
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/gallery/ajax_upload")
+	@ResponseBody
+	public Map<String, Object> ajaxUpload(GalleryDto dto, HttpServletRequest request) {
+		// 서비스를 이용해서 업로드된 이미지를 저장
+		service.saveImage(dto, request);
+		// {"isSuccess" : true} 형식의 json 문자열 응답
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("isSuccess", true);
+		return map;
+	}
+	
+	@RequestMapping("/gallery/list")
+	public String getList(HttpServletRequest request) {
+		service.getList(request);
+		return "gallery/list";
+	}
 }
